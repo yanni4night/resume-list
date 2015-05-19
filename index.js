21867a9cd3d1fe701fb502bfea8de3fba1632d20
yinyong02 <yinyong02@baidu.com>
2015-05-19 22:04:28 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2015 tieba.baidu.com
+ * index.js
+ *
+ * changelog
+ * 2015-05-19[21:34:55]:revised
+ *
+ * @author yinyong02@baidu.com
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+var async = require('async');
+
+var collect =  function () {
+
+    async.series([
+        function (cb) {
+            require('./lib/convert')(cb);
+        },
+        function (cb) {
+            require('./lib/list')(cb);
+        }
+    ], function () {});
+
+};
+
+collect();
+
+module.exports = collect;
