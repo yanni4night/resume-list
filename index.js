a905c7aeea6e588cff0719977eaee82ec20c392f
yinyong02 <yinyong02@baidu.com>
2015-05-25 15:32:19 +0800

    按子目录搜索

--- index.js
+++ index.js
@@ -10,18 +10,37 @@
  * @since 0.1.0
  */
 var async = require('async');
+var grunt = require('grunt');
+var path = require('path');
 
-var collect =  function () {
+var args = process.argv;
+
+var subDir = args[2];
+
+if (!grunt.file.isDir(path.join('resumes', subDir))) {
+    console.error(subDir, 'not found');
+    process.exit(-1);
+}
+
+var collect = function () {
+
+    /*
+            var subDirs = grunt.file.expand({
+                cwd: 'resumes'
+            }, '*').filter(function (dir) {
+                return grunt.file.isDir(path.join('resumes', dir));
+            });
+    */
 
     async.series([
         /*function (cb) {
             require('./lib/convert')(cb);
         },*/
         function (cb) {
-            require('./lib/list')(cb);
+            require('./lib/list')(subDir, cb);
         },
         function (cb) {
-            require('./lib/gen')(cb);
+            require('./lib/gen')(subDir, cb);
         }
     ], function () {});
 
